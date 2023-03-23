4e78ec467e74220b2da9a68da9454b5d64239fb7
Rabbivoka9 <rabbivoka9@gmail.com>
2023-03-23 16:29:17 +0100

    deuxieme commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import './App.css';
-// import Contenaire from './composant/contenaire';
-// import Forme from './composant/forme';
-// import Form2 from './composant/forme2';
-// import { Link } from 'react-router-dom'
 import Robos from './composant/Robos';
 
 function App() {
   return (
     <div className="App">
       <h1 className="title">MES AMIS ROBOTS</h1>
-      {/* <Contenaire/> */}
       <Robos />
     </div>
   );
